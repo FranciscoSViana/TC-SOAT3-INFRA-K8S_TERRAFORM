22f70f20d0ed5e96ab26fc10427fcfd38179d104
Francisco Viana <franciscosantos29@gmail.com>
2024-03-10 15:01:17 -0300

    Ajustes na cariação do cluster

--- eks/vpc.tf
+++ eks/vpc.tf
@@ -10,8 +10,8 @@ module "vpc" {
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
 
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
-  enable_dns_hostnames = true
+  enable_nat_gateway      = true
+  single_nat_gateway      = true
+  enable_dns_hostnames    = true
   map_public_ip_on_launch = true
 }
